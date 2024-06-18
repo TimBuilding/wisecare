a2b38b259ed6e9d62cbd69b29f76ad759ed6b434
xdjeremy <jeremy.fran@icloud.com>
2024-06-18 01:56:12 +0000

    TIM-273 fix(layout): navbar height (#57)

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -6,7 +6,7 @@ import Navigation from './navigation'
 
 const Navbar = () => {
   return (
-    <nav className="flex min-h-screen w-72 flex-col bg-navigation text-navigation-foreground">
+    <nav className="flex h-full min-h-screen w-72 flex-col bg-navigation text-navigation-foreground">
       <div className="flex flex-row items-center justify-between px-6 py-5">
         <Image src={WisecareLogo} alt="Wisecare Logo" height={32} />
         <Button variant={'ghost'} size={'icon'}>
