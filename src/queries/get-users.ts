27f6491a575619add374099bb84cfaf107fd5b27
xdjeremy <jeremy.fran@icloud.com>
2024-08-31 05:40:41 +0000

    TIM-358 fix(users): list fetching (#105)

--- src/queries/get-users.ts
+++ src/queries/get-users.ts
@@ -1,16 +1,14 @@
 import { TypedSupabaseClient } from '@/types/typedSupabaseClient'
 
 const getUsers = (supabase: TypedSupabaseClient) => {
-  return (
-    supabase
-      .from('user_profiles')
-      .select('user_id,first_name,last_name,departments(name)', {
-        count: 'exact',
-      })
-      .neq('departments.name', 'agent')
-      // .order('first_name')
-      .throwOnError()
-  )
+  return supabase
+    .from('user_profiles')
+    .select('user_id, first_name, last_name, departments!inner(name)', {
+      count: 'exact',
+    })
+    .neq('departments.name', 'agent')
+    .order('created_at', { ascending: false })
+    .throwOnError()
 }
 
 export default getUsers
