d871c477ab7afef93b903e1d41b3a12432cb0bf3
tamrivera05 <tamangelarivera@gmail.com>
2024-09-03 04:35:08 +0000

    TIM-364 Create Company Personnel List (#121)

--- src/app/(dashboard)/(home)/accounts/(Personnel)/[id]/employees-information.tsx
+++ src/app/(dashboard)/(home)/accounts/(Personnel)/[id]/employees-information.tsx
@@ -2,30 +2,121 @@ import React, { FC } from 'react'
 import { createBrowserClient } from '@/utils/supabase'
 import getEmployeeInputs from '@/queries/get-employee-inputs'
 import { useQuery } from '@supabase-cache-helpers/postgrest-react-query'
+import { Tables } from '@/types/database.types'
 
 interface EmployeesInformationProps {
-  id: string
+  data: Tables<'company_employees'>
 }
 
-const EmployeesInformation: FC<EmployeesInformationProps> = ({ id }) => {
-  const supabase = createBrowserClient()
-  const { data: employees, error } = useQuery(getEmployeeInputs(supabase, id))
-
-  console.log(employees)
-  console.log(error)
+const EmployeesInformation: FC<EmployeesInformationProps> = ({ data }) => {
   return (
     <>
-      {employees &&
-        employees.map((employee) => (
-          <div key={employee.id} className="flex flex-col pt-4 lg:p-2">
-            <span className="text-md font-semibold text-[#161a1d]">
-              {employee.first_name}
-            </span>
-            <span className="text-sm font-medium text-[#64748b]">
-              {employee.last_name}
-            </span>
-          </div>
-        ))}
+      <div className="col-span-4 flex grid-cols-6 flex-col pt-4 lg:grid lg:p-2">
+        <div className="flex flex-col gap-2 lg:col-span-2">
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data.employee_number}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">
+            EMPLOYEE NUMBER
+          </span>
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data?.real_description}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">
+            REAL DESCRIPTION
+          </span>
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data?.age}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">
+            AGE AS OF COVERAGE START
+          </span>
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data.birth_date}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">BIRTHDATE</span>
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data.gender}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">GENDER</span>
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data.civil_status}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">
+            CIVIL STATUS
+          </span>
+        </div>
+        <div className="flex flex-col gap-2 lg:col-span-2">
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data.bill_care_of}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">
+            BILL CARE OF
+          </span>
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data.bill_address}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">
+            BILL ADDRESS
+          </span>
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data.bill_city_municipal}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">
+            BILL MUNICIPAL
+          </span>
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data.bill_province}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">
+            BILL PROVINCE
+          </span>
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data.email}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">
+            EMAIL ADDRESS
+          </span>
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data.telephone_number}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">
+            TELEPHONE NUMBER
+          </span>
+        </div>
+        <div className="flex flex-col gap-2 lg:col-span-2">
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data.mobile_number}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">
+            MOBILE NUMBER
+          </span>
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data.agent_name}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">AGENT NAME</span>
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data.philhealth}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">PHILHEALTH</span>
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data.payment_mode}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">
+            PAYMENT MODE
+          </span>
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data.plan_type}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">PLAN TYPE</span>
+          <span className="text-md font-semibold text-[#161a1d]">
+            {data.plan_description}
+          </span>
+          <span className="text-sm font-medium text-[#64748b]">
+            PLAN DESCRIPTION
+          </span>
+        </div>
+      </div>
     </>
   )
 }
--- src/app/(dashboard)/(home)/accounts/(Personnel)/[id]/employees-page.tsx
+++ src/app/(dashboard)/(home)/accounts/(Personnel)/[id]/employees-page.tsx
@@ -7,39 +7,60 @@ import {
 } from '@/components/ui/collapsible'
 import { ChevronsUpDown } from 'lucide-react'
 import { Button } from '@/components/ui/button'
-// import { createBrowserClient } from '@/utils/supabase'
-// import { useQuery } from '@supabase-cache-helpers/postgrest-react-query'
-// import getEmployeeInputs from '@/queries/get-employee-inputs'
+import { createBrowserClient } from '@/utils/supabase'
+import { useQuery } from '@supabase-cache-helpers/postgrest-react-query'
+import getEmployeeInputs from '@/queries/get-employee-inputs'
+import { Tables } from '@/types/database.types'
 
 interface Props {
   companyId: string
 }
 
 const EmployeesPage: FC<Props> = ({ companyId }) => {
-  // const supabase = createBrowserClient()
-  // const { data: employees, error } = useQuery(getEmployeeInputs(supabase, companyId))
+  const supabase = createBrowserClient()
+  const { data: employees, isPending } = useQuery(
+    getEmployeeInputs(supabase, companyId),
+  )
+
   return (
     <div className="mx-auto flex w-full flex-col items-center justify-between gap-6">
-      <Collapsible
-        className={
-          'mx-auto w-full rounded-2xl border border-slate-200 bg-background p-6 drop-shadow-md'
-        }
-      >
-        <CollapsibleTrigger asChild={true}>
-          <Button
-            variant="ghost"
-            className="w-full max-w-5xl items-center justify-between p-0 hover:bg-transparent "
+      {isPending &&
+        [1, 2, 3].map((i) => (
+          <Collapsible
+            key={i}
+            className={
+              'mx-auto w-full rounded-2xl border border-slate-200 bg-background p-6 drop-shadow-md'
+            }
+          ></Collapsible>
+        ))}
+      {employees &&
+        employees.map((employee) => (
+          <Collapsible
+            key={employee.id}
+            className={
+              'mx-auto w-full rounded-2xl border border-slate-200 bg-background p-6 drop-shadow-md'
+            }
           >
-            <span className="text-lg font-semibold">Tamara Rivera</span>
-            <ChevronsUpDown className="justify-end" />
-          </Button>
-        </CollapsibleTrigger>
-        <CollapsibleContent>
-          <div className="grid-cols-3 lg:grid ">
-            <EmployeesInformation id={companyId} />
-          </div>
-        </CollapsibleContent>
-      </Collapsible>
+            <CollapsibleTrigger asChild={true}>
+              <Button
+                variant="ghost"
+                className="w-full max-w-5xl items-center justify-between p-0 hover:bg-transparent "
+              >
+                <span className="text-lg font-semibold">
+                  {employee.first_name} {employee.last_name}
+                </span>
+                <ChevronsUpDown className="justify-end" />
+              </Button>
+            </CollapsibleTrigger>
+            <CollapsibleContent>
+              <div className="grid-cols-3 lg:grid">
+                <EmployeesInformation
+                  data={employee as Tables<'company_employees'>}
+                />
+              </div>
+            </CollapsibleContent>
+          </Collapsible>
+        ))}
     </div>
   )
 }
