4a0e4ef7310197a28854744c3d5143cd9f65060b
tamrivera05 <tamangelarivera@gmail.com>
2024-10-24 06:02:23 +0000

    TIM-438 Change hover color of buttons (#206)

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -12,8 +12,7 @@ const buttonVariants = cva(
         default: 'bg-primary text-primary-foreground hover:bg-primary/90',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive-hover',
-        outline:
-          'border border-input bg-background hover:bg-accent hover:bg-primary',
+        outline: 'border border-input bg-background hover:bg-foreground/10',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
