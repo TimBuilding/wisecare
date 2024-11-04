f12a5d145c49340af9be9a7b507fa711603c84dd
xdjeremy <jeremy.fran@icloud.com>
2024-11-04 14:48:10 +0000

    feat(employees): implement employee import functionality (#321)

--- src/app/(dashboard)/(home)/accounts/(Personnel)/[id]/(employees)/import/import-employees.tsx
+++ src/app/(dashboard)/(home)/accounts/(Personnel)/[id]/(employees)/import/import-employees.tsx
@@ -1,33 +1,80 @@
 'use client'
+import { useCompanyContext } from '@/app/(dashboard)/(home)/accounts/(Personnel)/[id]/(company profile)/company-provider'
 import importFields from '@/app/(dashboard)/(home)/accounts/(Personnel)/[id]/(employees)/import/fields'
 import parseDate from '@/app/(dashboard)/(home)/accounts/(Personnel)/[id]/(employees)/import/parseDate'
 import parseRow from '@/app/(dashboard)/(home)/accounts/(Personnel)/[id]/(employees)/import/parseRow'
-import { Button } from '@/components/ui/button'
-import { format } from 'date-fns'
-import dynamic from 'next/dynamic'
-import { useState } from 'react'
+import { useToast } from '@/components/ui/use-toast'
+import { createBrowserClient } from '@/utils/supabase'
+import { useInsertMutation } from '@supabase-cache-helpers/postgrest-react-query'
 import { ReactSpreadsheetImport } from 'react-spreadsheet-import'
+import { Result } from 'react-spreadsheet-import/types/types'
+import { v4 as uuidv4 } from 'uuid'
 
 interface ImportEmployeesProps {
   isOpen: boolean
   setIsOpen: (isOpen: boolean) => void
 }
+
 const ImportEmployees = ({ isOpen, setIsOpen }: ImportEmployeesProps) => {
-  const onSubmit = (data: any) => {
-    console.log(data)
+  const supabase = createBrowserClient()
+  const { toast } = useToast()
+  const { accountId } = useCompanyContext()
+
+  const { mutateAsync } = useInsertMutation(
+    // @ts-ignore
+    supabase.from('pending_company_employees'),
+    ['id'],
+    null,
+    {
+      onSuccess: () => {
+        toast({
+          title: 'Employees imported successfully',
+          description: 'Employees are now pending approval',
+        })
+      },
+      onError: () => {
+        toast({
+          title: 'Error importing employees',
+          description: 'Please try again',
+        })
+      },
+    },
+  )
+
+  const handleSubmit = async (data: Result<any>) => {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser()
+    if (!user) throw new Error('User not found')
+
+    const employees = data.validData.map((employee) => ({
+      account_id: accountId,
+      first_name: employee.first_name,
+      last_name: employee.last_name,
+      birth_date: employee.birth_date,
+      gender: employee.gender,
+      civil_status: employee.civil_status,
+      card_number: employee.card_number,
+      effective_date: employee.effective_date,
+      room_plan: employee.room_plan,
+      maximum_benefit_limit: employee.maximum_benefit_limit,
+      created_by: user.id,
+      operation_type: 'insert',
+      batch_id: uuidv4(),
+    }))
+
+    await mutateAsync(employees)
   }
 
   return (
-    <>
-      <ReactSpreadsheetImport
-        isOpen={isOpen}
-        onClose={() => setIsOpen(false)}
-        onSubmit={onSubmit}
-        fields={importFields}
-        rowHook={parseRow}
-        uploadStepHook={parseDate}
-      />
-    </>
+    <ReactSpreadsheetImport
+      isOpen={isOpen}
+      onClose={() => setIsOpen(false)}
+      onSubmit={handleSubmit}
+      fields={importFields}
+      rowHook={parseRow}
+      uploadStepHook={parseDate}
+    />
   )
 }
 
