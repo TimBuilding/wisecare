9fcaf033b761b9b138f12d22eaf8f1769ab763b5
tamrivera05 <tamangelarivera@gmail.com>
2024-11-10 10:59:21 +0000

    TIM-690 Add count for account export requests (#368)

--- src/app/(dashboard)/admin/approval-request/account-exports/account-export-requests-table.tsx
+++ src/app/(dashboard)/admin/approval-request/account-exports/account-export-requests-table.tsx
@@ -1,24 +1,33 @@
+'use client'
+
 import React from 'react'
 import {
   PageDescription,
   PageHeader,
   PageTitle,
 } from '@/components/page-header'
+import { createBrowserClient } from '@/utils/supabase'
+import getPendingAccountExports from '@/queries/get-pending-account-exports'
+import { useQuery } from '@supabase-cache-helpers/postgrest-react-query'
+import { Skeleton } from '@/components/ui/skeleton'
 
 const AccountExportRequestsTable = () => {
+  const supabase = createBrowserClient()
+  // TODO: Add data from table
+  const { count, isPending } = useQuery(
+    getPendingAccountExports(supabase, 'accounts'),
+  )
   return (
     <div className="flex flex-col">
       <PageHeader>
         <div className="flex w-full flex-col gap-6 sm:flex-row sm:justify-between">
           <div>
             <PageTitle>Pending Account Exports</PageTitle>
-            {/*TODO: Add Loading state and count of export requests*/}
-            {/*{isPending ? (*/}
-            {/*  <Skeleton className="h-4 w-20" />*/}
-            {/*) : (*/}
-            {/*  <PageDescription> export requests</PageDescription>*/}
-            {/*)}*/}
-            <PageDescription> export requests</PageDescription>
+            {isPending ? (
+              <Skeleton className="h-4 w-20" />
+            ) : (
+              <PageDescription> {count} export requests</PageDescription>
+            )}
           </div>
           <div>
             {/*TODO: Implement TableSearch component*/}
